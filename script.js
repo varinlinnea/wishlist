67be6163d26be20db8eb564d2e4ca6ab689c47aa
varinlinnea <varinlinnea@gmail.com>
2025-03-13 00:49:51 +0100

    !

---
+++ script.js
@@ -0,0 +1,130 @@
+let model = {
+    newItem: {
+        category: '',
+        webURL: '',
+        imgURL: '',
+        name: ''
+    },
+    wishlist: JSON.parse(localStorage.getItem('wishlist')) || [],
+    isAdding: false,
+    categories: ["accessories", "clothing", "home", "tech", "other"]
+};
+
+
+function updateView() {
+    document.getElementById('app').innerHTML = /*HTML*/`
+        <div class="topNav">
+            <h1>Wishlist</h1>
+            ${createCategoryDropdown()}
+        </div>
+        ${createAddItem()}
+        ${showWishlist()}
+    `;
+}
+
+function createAddItem(){
+    if(!model.isAdding) return '<button onclick="startAdd()">Add item</button>';
+    return /*HTML*/`
+        <form>
+            <label for="name">Item name</label>
+            <input
+                type="text"
+                name ="name"
+                onchange="model.newItem.name = this.value"
+                value="${model.newItem.name ?? ''}">
+
+            <label for="category">Category</label>
+            <select name="category" onchange="model.newItem.category = this.value" value="${model.newItem.category ?? ''}">
+                <option value="accessories">accessories</option>
+                <option value="clothing">clothing</option>
+                <option value="home">home</option>
+                <option value="tech">tech</option>
+                <option value="other">other</option>
+            </select>
+
+            <label for="link">Website URL</label>
+            <input
+                type="url"
+                name="link"
+                onchange="model.newItem.webURL = this.value"
+                value="${model.newItem.webURL ?? ''}">
+            
+            <label for="picture">Image URL</label>
+            <input
+                type="url"
+                name="picture"
+                onchange="model.newItem.imageURL = this.value"
+                value="${model.newItem.imageURL ?? ''}">
+            
+            <button onclick="addItem()">Add to wishlist</button>
+            <button onclick="cancelAddItem()">Cancel</button>
+        </form>
+    `;
+}
+
+function showWishlist() {
+    let wishlistHtml = `<div class="wishlistContainer">`;
+    let index = 0;
+    for (let item of model.wishlist) {
+        wishlistHtml += /*HTML*/`
+            <div class="wishlistElement">
+                <div class="leftContainer">
+                    <div>${item.category}</div>
+                    <div>${item.name}</div>
+                    <a href="${item.webURL}">Link to website</a>
+                </div>
+                <img class="itemImg" src="${item.imageURL}">
+                <button onclick="deleteItem(${index})">x</button>
+            </div>
+        `;
+        index++;
+    }
+    wishlistHtml += '</div>';
+    return wishlistHtml;
+}
+
+function createCategoryDropdown() {
+    let categoryHTML = /*HTML*/`<div class="dropdown">
+        <button onclick="openCategoryDropdown()" class="dropBtn">Categories</button>
+        <div id="categoryDropdown" class="dropdownContent">
+        `;
+
+    for (let category of model.categories) {
+        categoryHTML += /*HTML*/`<div onclick="sortByCategory(category)">${category}</div>`;
+    }
+    categoryHTML += '</div></div>';
+    return categoryHTML;
+}
+
+function addItem() {
+    model.wishlist.push(model.newItem);
+    model.isAdding = false;
+    model.newItem = {};
+    saveWishlist();
+    updateView();
+}
+
+function startAdd() {
+    model.isAdding = true;
+    updateView();
+}
+
+function cancelAddItem() {
+    model.isAdding = false;
+    updateView();
+}
+
+function deleteItem(index) {
+    model.wishlist.splice(index, 1);
+    saveWishlist();
+    updateView();
+}
+
+function saveWishlist() {
+    localStorage.setItem('wishlist', JSON.stringify(model.wishlist));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    model.wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+    updateView();
+});
